3244f50e22f318973f50af5f5408e17c81ad4001
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-08 16:36:48 +0700

    feat: implement type casting of AnimeList and SuggestionAnime

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,9 +1,11 @@
+import type { AnimeList } from "@/types/anime.type";
+
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const fetchAnimeList = async (category: string, page = 1) => {
-  const data = await fetch(
+  const response = await fetch(
     `${BASE_URL}/${category}?page=${page}`
   );
-  const results = await data.json();
-  return results;
+  const { results } = await response.json();
+  return results as AnimeList[];
 };
