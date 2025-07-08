3006c151b60bebf12a67e0a1fb9c88e1e21970a2
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-08 16:16:47 +0700

    feat: create component for AnimeRow and implement to index route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,39 @@
-import { Welcome } from "../welcome/welcome";
+import AnimeRow from "@/components/AnimeRow";
 
 export default function Home() {
-	return <Welcome />;
+	return (
+		<>
+			<AnimeRow
+				rowTitle={"Top Anime"}
+				category={"most-popular"}
+				animeType={"title"}
+				toPage={"popular"}
+				key={"popular"}
+			/>
+
+			<AnimeRow
+				rowTitle={"Recent Episodes"}
+				category={"recent-episodes"}
+				animeType={"title"}
+				toPage={"latest"}
+				key={"latest"}
+			/>
+
+			<AnimeRow
+				rowTitle={"Top Airing"}
+				category={"top-airing"}
+				animeType={"title"}
+				toPage={"trending"}
+				key={"trending"}
+			/>
+
+			<AnimeRow
+				rowTitle={"Anime Movies"}
+				category={"anime-movies"}
+				animeType={"title"}
+				toPage={"movies"}
+				key={"movies"}
+			/>
+		</>
+	);
 }
