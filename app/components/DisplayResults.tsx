2d952653cc9d798693c025e8dfc65b60749f584b
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-09 21:32:22 +0700

    fix: search results positioning

--- app/components/DisplayResults.tsx
+++ app/components/DisplayResults.tsx
@@ -4,67 +4,43 @@ import type { AnimeList } from "@/types/anime.type";
 
 interface Props {
 	searchResults: AnimeList[];
-	searchQuery: string;
 	loading: boolean;
 }
 
-const DisplayResults = ({ searchResults, searchQuery, loading }: Props) => {
+const DisplayResults = ({ searchResults, loading }: Props) => {
 	return (
-		searchResults && (
-			<div
-				className={
-					searchQuery !== ""
-						? "absolute bottom-[-1] duration-500"
-						: "hidden duration-500"
-				}
-			>
-				<div className="lg:w-full max-h-[70vh] flex flex-col justify-center items-center overflow-auto bg-[#0a192f] rounded-lg px-2">
-					{loading ? (
-						<div className="flex w-48 xl:w-72 flex-col items-center justify-center p-4">
-							<BiLoaderAlt size={32} color="slate" className="animate-spin" />
-						</div>
-					) : searchResults.length > 0 ? (
-						searchResults.map((result) => (
-							<div
-								className="w-full gap-3 py-2 px-4 cursor-pointer hover:bg-[#0f2647]/30 duration-400"
-								onClick={() => {
-									// dispatch(selectAnime(result.title));
-									// dispatch(goToEpisode(1));
-								}}
-							>
-								<NavLink
-									to={`/anime/${result.id}`}
-									key={result.title}
-									className="flex space-x-2"
-								>
-									<img
-										src={result.image}
-										alt={result.title}
-										className="rounded-lg object-cover w-16 h-20"
-									/>
-									{/* <Image
-										height={80}
-										width={60}
-										src={result.image}
-										className="rounded-lg object-cover"
-									/> */}
-									<p className="text-xs lg:text-sm text-slate-200 text-left whitespace-normal text-ellipsis max-w-[12rem]">
-										{result.title}
-									</p>
-								</NavLink>
-							</div>
-						))
-					) : (
-						<div
-							className="grid place-content-center
+		<ul className="absolute top-12 left-0 w-full bg-[#0a192f] flex flex-col rounded-lg shadow-lg max-h-80 overflow-y-auto">
+			{loading ? (
+				<li className="flex w-48 xl:w-72 flex-col items-center justify-center p-4">
+					<BiLoaderAlt size={32} color="slate" className="animate-spin" />
+				</li>
+			) : searchResults.length > 0 ? (
+				searchResults.map((result) => (
+					<li
+						className="w-full gap-3 py-2 px-4 hover:bg-[#0f2647]/30 duration-400"
+						key={result.title}
+					>
+						<NavLink to={`/anime/${result.id}`} className="flex space-x-2">
+							<img
+								src={result.image}
+								alt={result.title}
+								className="rounded-lg object-cover w-16 h-20"
+							/>
+							<p className="text-xs lg:text-sm text-slate-200 text-left whitespace-normal text-ellipsis max-w-[12rem]">
+								{result.title}
+							</p>
+						</NavLink>
+					</li>
+				))
+			) : (
+				<div
+					className="grid place-content-center
                place-items-center h-24 w-full p-4"
-						>
-							<p className="text-slate-200">No Anime Found</p>
-						</div>
-					)}
+				>
+					<p className="text-slate-200">No Anime Found</p>
 				</div>
-			</div>
-		)
+			)}
+		</ul>
 	);
 };
 
--- app/components/SearchComponent.tsx
+++ app/components/SearchComponent.tsx
@@ -1,54 +1,73 @@
 import { useCallback, useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import DisplayResults from "./DisplayResults";
 import { useDebounceValue } from "usehooks-ts";
+import { useSearchParams } from "react-router";
 import { fetchAnimeBySearch } from "@/lib/api";
 import type { AnimeList } from "@/types/anime.type";
+import DisplayResults from "./DisplayResults";
 
 const SearchComponent = () => {
-	const [searchQuery, setSearchQuery] = useState("");
+	const [searchText, setSearchText] = useState("");
 	const [searchResults, setSearchResults] = useState<AnimeList[]>([]);
 	const [loading, setLoading] = useState(false);
+	const [searchParams, setSearchParams] = useSearchParams();
 
-	const [debouncedSearchQuery] = useDebounceValue(searchQuery, 500);
+	const [debouncedSearchText] = useDebounceValue(searchText, 500);
+
+	const searchQuery = searchParams.get("search") || "";
 
 	const fetchData = useCallback(
 		async (query: string) => {
-			const results = await fetchAnimeBySearch(query);
-			setSearchResults(results);
-			setLoading(false);
+			try {
+				setLoading(true);
+				setSearchParams({ search: debouncedSearchText });
+
+				const results = await fetchAnimeBySearch(query);
+				setSearchResults(results);
+			} catch (error) {
+				console.error("Error fetching search results:", error);
+				setSearchResults([]);
+			} finally {
+				setLoading(false);
+			}
 		},
-		[debouncedSearchQuery]
+		[debouncedSearchText]
 	);
 
 	useEffect(() => {
-		if (debouncedSearchQuery.length >= 3) {
-			fetchData(debouncedSearchQuery);
+		if (debouncedSearchText.length >= 3) {
+			fetchData(debouncedSearchText);
+		} else {
+			setSearchParams({});
 		}
-	}, [fetchData, debouncedSearchQuery]);
+	}, [fetchData, debouncedSearchText]);
 
 	return (
 		<div className="relative max-w-md mr-4">
 			<div
-				className={`flex items-center bg-transparent px-4 py-2 transition-all
-				border-b border-slate-600 ${searchQuery ? "w-72" : "w-48 focus-within:w-72"}`}
+				className={`flex items-center bg-transparent group px-4 py-2 transition-all
+				border-b border-slate-600 ${searchText ? "w-72" : "w-48 focus-within:w-72"}`}
 			>
 				<input
 					type="text"
 					placeholder="Search anime..."
-					value={searchQuery}
-					onChange={(e) => setSearchQuery(e.target.value)}
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+					onBlur={() => {
+						setSearchParams({});
+					}}
+					onFocus={() => {
+						if (searchText.length >= 3) {
+							setSearchParams({ search: searchText });
+						}
+					}}
 					className="bg-transparent outline-none focus:text-slate-300 text-slate-600 w-full placeholder-slate-400"
 				/>
 				<FaSearch className="text-slate-400 mr-2" />
 			</div>
 
 			{searchQuery.length >= 3 && (
-				<DisplayResults
-					loading={loading}
-					searchResults={searchResults}
-					searchQuery={searchQuery}
-				/>
+				<DisplayResults loading={loading} searchResults={searchResults} />
 			)}
 		</div>
 	);
