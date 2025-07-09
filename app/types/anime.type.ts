aaa9b9c4f983ec3258653e818e0c8f3306b3ff3f
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-09 21:44:05 +0700

    feat: add AnimeEpisode, AnimeRecommendation, AnimeRelated, and AnimeDetail interfaces

--- app/types/anime.type.ts
+++ app/types/anime.type.ts
@@ -24,3 +24,62 @@ export interface SuggestionAnime {
   description: string;
   totalEpisodes: number;
 }
+
+export interface AnimeEpisode {
+  id: string;
+  number: number;
+  title: string;
+  isFiller: boolean;
+  isSubbed: boolean;
+  isDubbed: boolean;
+  url: string;
+}
+
+export interface AnimeRecommendation {
+  id: string;
+  title: string;
+  url: string;
+  image: string;
+  duration: string;
+  watchList: string;
+  japaneseTitle: string;
+  type: string;
+  nsfw: boolean;
+  sub: number;
+  dub: number;
+  episodes: number;
+}
+
+export interface AnimeRelated {
+  id: string;
+  title: string;
+  url: string;
+  image: string;
+  japaneseTitle: string;
+  type: string;
+  sub: number;
+  dub: number;
+  episodes: number;
+}
+
+export interface AnimeDetail {
+  id: string;
+  title: string;
+  malID: number;
+  alID: number;
+  japaneseTitle: string;
+  image: string;
+  description: string;
+  type: string;
+  url: string;
+  recommendations: AnimeRecommendation[];
+  relatedAnime: AnimeRelated[];
+  subOrDub: string;
+  hasSub: boolean;
+  hasDub: boolean;
+  genres: string[];
+  status: string;
+  season: string;
+  totalEpisodes: number;
+  episodes: AnimeEpisode[];
+}
