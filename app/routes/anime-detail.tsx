03c576f658a32a247fe03e89310855828730184f
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-09 22:28:02 +0700

    feat: add and integrate AnimeDescription component

--- app/routes/anime-detail.tsx
+++ app/routes/anime-detail.tsx
@@ -1,5 +1,7 @@
 import { fetchAnimeDetail } from "@/lib/api";
 import type { Route } from "./+types/anime-detail";
+import AnimeDescription from "@/components/AnimeDescription";
+import type { AnimeDetail } from "@/types/anime.type";
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
 	const animeDetail = await fetchAnimeDetail(params.id);
@@ -22,9 +24,38 @@ export function meta(loaderData: Route.MetaArgs) {
 }
 
 const AnimeDetailPage = ({ loaderData }: Route.ComponentProps) => {
-	console.log("Loader Data:", loaderData.animeDetail);
+	const { animeDetail }: { animeDetail: AnimeDetail } = loaderData;
 
-	return <div>Anime Detail</div>;
+	return (
+		<div>
+			<div
+				className="grid grid-cols-1 lg:grid-cols-[2fr_7fr_3fr] w-full h-auto lg:h-[90vh]
+					mx-auto px-4 lg:px-14 gap-6 md:gap-8 lg:gap-12 justify-center lg:justify-between"
+			>
+				<div className="order-1 lg:order-3 md:col-span-2 lg:col-span-1">
+					<AnimeDescription animeData={animeDetail} />
+				</div>
+
+				{/* <div className="order-2 lg:order-2">
+					<VideoComponent
+						title={
+							animeData.type !== "Movie"
+								? `${animeData.title} - Episode ${episode}`
+								: `${animeData.title}`
+						}
+						episodeDetail={episodeDetail}
+						synopsis={animeData.description}
+					/>
+				</div>
+
+				{animeData.type !== "Movie" && (
+					<div className="order-3 lg:order-1">
+						<Episodes animeData={animeData} currentEpisode={episode} />
+					</div>
+				)} */}
+			</div>
+		</div>
+	);
 };
 
 export default AnimeDetailPage;
