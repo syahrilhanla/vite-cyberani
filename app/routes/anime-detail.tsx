5b61165708ddda4f92a2251a11ed89b2013c3089
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-10 14:59:07 +0700

    feat: update Episodes component to use Redux state for current episode and refactor episode list rendering

--- app/routes/anime-detail.tsx
+++ app/routes/anime-detail.tsx
@@ -1,9 +1,13 @@
-import { fetchAnimeDetail } from "@/lib/api";
-import type { Route } from "./+types/anime-detail";
 import AnimeDescription from "@/components/AnimeDescription";
-import type { AnimeDetail } from "@/types/anime.type";
 import StreamingComponent from "@/components/StreamingComponent";
 import Episodes from "@/components/Episodes";
+
+import { fetchAnimeDetail } from "@/lib/api";
+
+import type { Route } from "./+types/anime-detail";
+import type { AnimeDetail } from "@/types/anime.type";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/lib/anime.store";
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
 	const animeDetail = await fetchAnimeDetail(params.id);
@@ -26,7 +30,10 @@ export function meta(loaderData: Route.MetaArgs) {
 }
 
 const AnimeDetailPage = ({ loaderData }: Route.ComponentProps) => {
-	const { animeDetail }: { animeDetail: AnimeDetail } = loaderData;
+	const { animeDetail } = loaderData as { animeDetail: AnimeDetail };
+	const { currentEpisode } = useSelector(
+		(state: RootState) => state.animeReducer
+	);
 
 	return (
 		<div>
@@ -35,7 +42,7 @@ const AnimeDetailPage = ({ loaderData }: Route.ComponentProps) => {
 					mx-auto px-4 lg:px-14 gap-6 md:gap-8 lg:gap-12 justify-center lg:justify-between"
 			>
 				<div className="order-3 lg:order-1">
-					<Episodes animeData={animeDetail} currentEpisode={1} />
+					<Episodes animeData={animeDetail} />
 				</div>
 
 				<div className="order-1 lg:order-3 md:col-span-2 lg:col-span-1">
@@ -44,8 +51,8 @@ const AnimeDetailPage = ({ loaderData }: Route.ComponentProps) => {
 
 				<div className="order-2 lg:order-2">
 					<StreamingComponent
-						title={`${animeDetail.title} - Episode ${1}`}
-						episodeDetail={animeDetail.episodes[0]}
+						title={`${animeDetail.title} - Episode ${currentEpisode}`}
+						episodeDetail={animeDetail.episodes[currentEpisode - 1]}
 						synopsis={animeDetail.description}
 					/>
 				</div>
