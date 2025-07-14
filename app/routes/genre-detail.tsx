ad1c00ee56f8360ff5896d890b0b9c6e0e7c5e36
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-14 11:03:28 +0700

    feat: correct data fetching in genre-detail page

--- app/routes/genre-detail.tsx
+++ app/routes/genre-detail.tsx
@@ -1,6 +1,7 @@
 import { fetchAnimeByGenre } from "@/lib/api";
 import type { Route } from "./+types/genre-detail";
 import AnimeCardShowroom from "@/components/AnimeCardShowroom";
+import Pagination from "@/components/Pagination";
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
 	const { genre } = params;
@@ -9,24 +10,25 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
 		throw new Response("Genre not found", { status: 404 });
 	}
 
-	const animeList = await fetchAnimeByGenre(genre);
+	const { results, totalPages } = await fetchAnimeByGenre(genre);
 
-	return animeList;
+	return { animeList: results, totalPages };
 };
 
 const GenreDetail = ({ loaderData, params }: Route.ComponentProps) => {
 	const { genre } = params;
-	const animeList = loaderData;
+	const { animeList, totalPages } = loaderData;
+
 	return (
-		<>
-			<div className="lg:w-[70%] ml-3 mb-4">
+		<div className="lg:w-[70%] my-6">
+			<div>
 				<h1 className="text-left text-slate-200 text-3xl font-semibold capitalize">
 					{genre}
 				</h1>
 			</div>
 			<div
-				className="lg:w-[70%] grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4
-		 mt-2 text-left font-medium text-slate-100 pb-2"
+				className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4
+				mt-2 mb-4 text-left font-medium text-slate-100 pb-2"
 			>
 				{/* if data results come as series and not as an episode than render AnimeShowroom */}
 				{animeList &&
@@ -35,12 +37,12 @@ const GenreDetail = ({ loaderData, params }: Route.ComponentProps) => {
 					))}
 			</div>
 
-			{/* <Pagination
-				currentPage={currentPage}
-				setCurrentPage={setCurrentPage}
+			<Pagination
+				currentPage={1}
+				setCurrentPage={() => {}}
 				totalPages={totalPages}
-			/> */}
-		</>
+			/>
+		</div>
 	);
 };
 
