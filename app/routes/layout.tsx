8d7dc6e30903da9f227886df38ddf58162ed4cfa
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-17 17:08:41 +0700

    fix: ensure Layout component occupies full width and height

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
 	return (
-		<div className="mt-20 overflow-hidden">
+		<div className="w-full h-full mt-20 overflow-hidden">
 			<Outlet />
 		</div>
 	);
