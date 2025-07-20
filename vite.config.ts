5234163509fe3d6470931b968fc185d55d788f50
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-20 20:24:07 +0700

    chore: ensure base path is set in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+  base: "/",
 });
