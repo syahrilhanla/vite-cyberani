7c1e0ad6e2effca8dcafc88e93bf0c05c42dd2e8
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-22 21:31:14 +0700

    chore: remove unused Netlify plugin and update Vercel rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   base: "/",
 });
