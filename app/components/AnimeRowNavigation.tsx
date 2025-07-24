1b43561b6ef5607fbbf51102b4566774bb1d5a86
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-24 10:40:47 +0700

    fix: refactor AnimeRow component and add AnimeRowNavigation for improved swiper controls

---
+++ app/components/AnimeRowNavigation.tsx
@@ -0,0 +1,27 @@
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { type SwiperClass } from "swiper/react";
+
+interface Props {
+	swiper: SwiperClass | null;
+}
+
+const AnimeRowNavigation = ({ swiper }: Props) => {
+	return (
+		<div className="absolute h-full -right-14 hidden 2xl:-right-10 lg:grid top-1/2 mt-1 py-2 -translate-y-1/2 gap-3 z-20">
+			<button
+				onClick={() => swiper?.slidePrev()}
+				className="bg-slate-800/80 h-full p-2 rounded-xl text-lg text-slate-200 shadow hover:bg-slate-700 transition"
+			>
+				<MdKeyboardArrowLeft size={28} />
+			</button>
+			<button
+				onClick={() => swiper?.slideNext()}
+				className="bg-slate-800/80 h-full p-2 rounded-xl text-lg text-slate-200 shadow hover:bg-slate-700 transition"
+			>
+				<MdKeyboardArrowRight size={28} />
+			</button>
+		</div>
+	);
+};
+
+export default AnimeRowNavigation;
